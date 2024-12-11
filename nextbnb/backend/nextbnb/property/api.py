ad9b696423e444fad59522b603e89b9b6064faa8
Md Imran <imranhacks59@gmail.com>
2024-12-11 08:23:40 +0530

    auth, add property and property details

--- nextbnb/backend/nextbnb/property/api.py
+++ nextbnb/backend/nextbnb/property/api.py
@@ -1,8 +1,9 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from .serializers import PropertiesListSerializer
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
 from .models import Property
+from .forms import PropertyForm
 
 @api_view(['GET'])
 @authentication_classes([])
@@ -16,4 +17,31 @@ def properties_list(request):
         'data': serializer.data,
         'favorites': []
     })
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_detail(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    serializer = PropertiesDetailSerializer(property, many=False)
+
+    return JsonResponse(serializer.data)
+
+@api_view(['POST', 'FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'success': True})
+    else:
+        print('error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+
+
 
