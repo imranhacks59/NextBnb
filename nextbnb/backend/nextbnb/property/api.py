07947d4f7ff9e623a88a7d1ed311ee2440635f29
Md Imran <imranhacks59@gmail.com>
2024-12-14 23:19:04 +0530

    landloard detail page

--- nextbnb/backend/nextbnb/property/api.py
+++ nextbnb/backend/nextbnb/property/api.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
-from .models import Property
+from .serializers import PropertiesListSerializer, PropertiesDetailSerializer, ReservationsListSerializer
+from .models import Property, Reservation
 from .forms import PropertyForm
 
 @api_view(['GET'])
@@ -28,6 +28,18 @@ def properties_detail(request, pk):
 
     return JsonResponse(serializer.data)
 
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
 @api_view(['POST', 'FILES'])
 def create_property(request):
     form = PropertyForm(request.POST, request.FILES)
@@ -41,6 +53,46 @@ def create_property(request):
     else:
         print('error', form.errors, form.non_field_errors)
         return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+@api_view(['POST'])
+def book_property(request,pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+
+        return JsonResponse({'is_favorite': False})
+    else:
+        property.favorited.add(request.user)
+
+        return JsonResponse({'is_favorite': True})
 
 
 
